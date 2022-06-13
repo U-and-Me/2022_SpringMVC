c60fd473e3ceff8f6ad10fa1251f326ff42b210c
U-and-Me <yunasun0920@gmail.com>
2022-06-13 15:13:02 +0900

    객체로 파라미터 받는 실습

---
+++ GetParameterObject/src/main/java/kr/hs/study/controller/TestController.java
@@ -0,0 +1,41 @@
+package kr.hs.study.controller;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class TestController {
+
+	@GetMapping("/test1")
+	public String test1(@RequestParam Map<String, String> map) {
+		
+		String data1 = map.get("data1");
+		String data2 = map.get("data2");
+		
+		System.out.println("data1 : " + data1);
+		System.out.println("data2 : " + data2);
+		
+		return "result";
+		
+	}
+	
+	@GetMapping("/test2")
+	public String test2(@RequestParam Map<String, String> map, @RequestParam(name="data3") List<String> list) {
+		
+		String data1 = map.get("data1");
+		String data2 = map.get("data2");
+		
+		System.out.println("data1 : " + data1);
+		System.out.println("data2 : " + data2);
+		
+		for(String str : list)
+			System.out.println("data3 : " + str);
+		
+		return "result";
+	}
+	
+}
---
+++ GetParameterObject/src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="EUC-KR">
+<title>Insert title here</title>
+</head>
+<body>
+	<h1>Hi Spring MVC</h1>
+	<a href="test1?data1=10&data2=20">test1 map 사용</a> <br>
+	<a href="test2?data1=10&data2=20&data3=30&data3=40">test2 List 사용</a>
+</body>
+</html>

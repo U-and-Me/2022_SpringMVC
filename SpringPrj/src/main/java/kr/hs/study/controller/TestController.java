4a48e2fbbf5c3d772d315f86a25c5d3ba0081d6a
U-and-Me <yunasun0920@gmail.com>
2022-06-29 14:01:15 +0900

    ModelAndView 적용

--- SpringPrj/src/main/java/kr/hs/study/controller/TestController.java
+++ SpringPrj/src/main/java/kr/hs/study/controller/TestController.java
@@ -1,25 +1,29 @@
 package kr.hs.study.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
 public class TestController {
 	
 	// 1부터 10까지의 합
-	@RequestMapping(value="/sum", method=RequestMethod.GET)
-	public String sum() {
+	@GetMapping(value="sum")
+	public ModelAndView sum(ModelAndView mv) {
 		int sum = 0;
 		for(int i = 1; i <= 10; i++)
 			sum += i;
 		System.out.println("1부터 10까지의 합 : " + sum);
 		
-		return "result";
+		mv.setViewName("sum");
+		mv.addObject("total", sum);
+		
+		return mv;
 	}
 	
-	@RequestMapping(value="/multi_table", method=RequestMethod.GET)
+	// 구구단
+	@GetMapping(value="/multi_table")
 	public String multi_table() {
 		
 				
